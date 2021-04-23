ffacaf11584d372db1a0ea74bd1db8eb9698a174
Alpay Palabıyık <alpayoguz97@gmail.com>
2021-04-23 22:49:47 +0300

    Second Commit

---
+++ Assets/PhiPhenomena.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhiPhenomena : MonoBehaviour
+{
+   
+
+    [SerializeField]
+    GameObject[] cubes;
+
+    void Start() => StartCoroutine(ChangeColor());
+    
+        
+
+    IEnumerator  ChangeColor()
+    {
+        while(true)
+        {
+            foreach (GameObject cube in cubes)
+            {
+                Renderer _cubeRenderer = cube.GetComponent<Renderer>();
+                _cubeRenderer.material.color = Color.red;
+                yield return new WaitForSeconds(0.09f);
+                _cubeRenderer.material.color = Color.white;
+            }
+
+        }
+       
+        
+    }
+
+}
